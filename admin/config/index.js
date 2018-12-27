b1a93acef6254724593e6af85bde8633ef66daad
LRY1994 <949892801@qq.com>
2018-12-27 18:07:34 +0800

    改admin端口

--- admin/config/index.js
+++ admin/config/index.js
@@ -6,7 +6,8 @@ const path = require('path')
 
 module.exports = {
   dev: {
-
+    disableHostCheck: true,
+    port: 8082,
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
@@ -14,7 +15,6 @@ module.exports = {
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
-    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
